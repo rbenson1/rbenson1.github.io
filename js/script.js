2ea3f0f7f5b3c6d33c4f016a4c2c9110e6fb0f83
rbenson1 <rbenson1@ithaca.edu>
2018-03-17 15:59:28 -0400

    First Push of Portfolio

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//Infinity Lab Pictures
+var il1 = "<div class='embed-responsive embed-responsive-16by9'><iframe src='https://player.vimeo.com/video/247087543' class='embed-responsive embed-responsive-item' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen> < /iframe> </div>";
+
+var il2 = "<img class='img-fluid' src='images/infinityLab/Table.jpg'>";
+
+var il3 = "<img class='img-fluid' src='images/infinityLab/Beaker_Outlets.jpg'>";
+
+var il4 = "<img class='img-fluid' src='images/infinityLab/Grate_Particles.jpg'>";
+
+//Emerging Media Lab Pictures
+
+var em1 = "<img class='img-fluid' src='images/emLab/EM_Lab01.jpg'>";
+
+var em2 = "<img class='img-fluid' src='images/emLab/EM_Lab02.jpg'>";
+
+var em3 = "<img class='img-fluid' src='images/emLab/EM_Lab03.jpg'>";
+
+var em4 = "<img class='img-fluid' src='images/emLab/EM_Lab04.jpg'>";
+
+//The Name of This Game is Classified Pictures
+
+var clas1 = " <div class='embed-responsive embed-responsive-16by9'> <iframe class = 'embed-responsive-video' src='https://www.youtube.com/embed/wyPHpSjWBcs' frameborder = '0' allow = 'autopla'; encrypted - media ' allowfullscreen></iframe> < /div > ";
+
+var clas2 = "<img class='img-fluid' src='images/tnotgic/notebook.png' alt='3D modeled table with beakers'>";
+
+var clas3 = "<img class='img-fluid' src='images/tnotgic/puzzle1.png'>";
+
+var clas4 = " <img class='img-fluid' src='images/tnotgic/painting.png'>";
+
+
+function imgSwitch(elementId, slideNum) {
+    document.getElementById(elementId).innerHTML = slideNum;
+
+}
